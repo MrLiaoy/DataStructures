02531a17c0cb0005fd53cb9688af8d17ad0aa563
MrLiaoy <53252631+MrLiaoy@users.noreply.github.com>
2019-08-01 09:39:41 +0800

    第一次：实现稀疏数组 第二次：对稀疏数组序列化和反序列化

--- src/com/mrl/sparseArray/SparseArray.java
+++ src/com/mrl/sparseArray/SparseArray.java
@@ -1,5 +1,7 @@
 package com.mrl.sparseArray;
 
+import java.io.*;
+
 public class SparseArray {
     public static void main(String[] args) {
         int[][] arr=new int[10][10];
@@ -8,6 +10,56 @@ public class SparseArray {
         arr[4][8]=4;
         arr[2][3]=4;
         arr[5][4]=4;
+        //将数组转换为稀疏数组
+        int[][] sparse = getSparse(arr);
+
+        //对数组进行序列化
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("sparase.data"));
+            objectOutputStream.writeObject(sparse);
+            objectOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i=0;i<4;i++){
+            System.out.println(sparse[i][0]+"\t"+sparse[i][1]+"\t"+sparse[i][2]+"\t");
+        }
+
+
+        //对保存的数据反序列化
+        ObjectInputStream objectInputStream=null;
+       int[][] spa=null;
+        try {
+
+            objectInputStream=new ObjectInputStream(new FileInputStream("sparase.data"));
+          spa= (int[][]) objectInputStream.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //将稀疏数组转换为普通数组
+        int[][] arr2 = getArray(spa);
+
+        for (int i=0;i<arr2.length;i++){
+            for(int j=0;j<arr2[i].length;j++){
+                System.out.print(arr2[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] getArray(int[][] sparse) {
+        int[][] arr2=new int[sparse[0][0]][sparse[0][1]];
+
+        for (int i=1;i<sparse.length;i++){
+            arr2[sparse[i][0]][sparse[i][1]]=sparse[i][2];
+        }
+        return arr2;
+    }
+
+    private static int[][] getSparse(int[][] arr) {
         int row=0;
         for(int i=0 ; i<arr.length;i++){
             for (int j=0;j<arr[i].length;j++){
@@ -35,22 +87,6 @@ public class SparseArray {
                 }
             }
         }
-
-        for (int i=0;i<4;i++){
-            System.out.println(sparse[i][0]+"\t"+sparse[i][1]+"\t"+sparse[i][2]+"\t");
-        }
-
-        int[][] arr2=new int[sparse[0][0]][sparse[0][1]];
-
-        for (int i=1;i<sparse.length;i++){
-            arr2[sparse[i][0]][sparse[i][1]]=sparse[i][2];
-        }
-
-        for (int i=0;i<arr2.length;i++){
-            for(int j=0;j<arr2[i].length;j++){
-                System.out.print(arr2[i][j]+"\t");
-            }
-            System.out.println();
-        }
+        return sparse;
     }
 }
